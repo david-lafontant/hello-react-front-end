da864bb8c641bb3913d8ac18f8ec29eaaee3b296
david-lafontant <lafontant.david@gmail.com>
2022-07-08 17:32:37 -0600

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greeting from './components/greeting/greeting';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Greeting />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
